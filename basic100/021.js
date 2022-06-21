12058742c71fb1f748f15d176acd0b6eccd9c52c
saejinpark951210 <saejinpark951210@gmail.com>
2022-06-21 18:04:52 +0900

    Create 021.js

---
+++ basic100/021.js
@@ -0,0 +1,26 @@
+// 알파벳과 숫자로 이루어진 단어 1개가 입력된다.
+// 입력받은 단어의 각 문자를 한 줄에 한 문자씩 분리해 출력한다.
+
+// 예시
+// s = input()
+// print(s[0])
+// print(s[1])
+// ...
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (line) => {
+  for (let x of line.split("")) {
+    console.log(x);
+  }
+  rl.close();
+});
+
+rl.on("close", () => {
+  process.exit();
+});
