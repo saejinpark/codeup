354989362e739a5c9d760a66927b16c003ba8169
saejinpark951210 <saejinpark951210@gmail.com>
2022-06-23 00:26:47 +0900

    Create 053.js

---
+++ basic100/053.js
@@ -0,0 +1,22 @@
+// 정수가 입력되었을 때, True/False 로 평가해주는 프로그램을 작성해보자.
+
+// 예시
+// n = int(input())
+// print(bool(n))
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (line) => {
+  let a = Number(line);
+  console.log(!Boolean(a));
+  rl.close();
+});
+
+rl.on("close", () => {
+  process.exit();
+});
