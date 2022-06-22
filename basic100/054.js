2f0d8007d827d13bd93ef020ad01685fb1c7ac70
saejinpark951210 <saejinpark951210@gmail.com>
2022-06-23 00:26:49 +0900

    Create 054.js

---
+++ basic100/054.js
@@ -0,0 +1,25 @@
+// 2개의 정수값이 입력될 때,
+// 그 불 값이 모두 True 일 때에만 True 를 출력하는 프로그램을 작성해보자.
+
+// 예시
+// a, b = input().split()
+// print(bool(int(a)) and bool(int(b)))
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (line) => {
+  let arr = line.split(" ").map((v) => Number(v));
+  let a = arr[0];
+  let b = arr[1];
+  console.log(Boolean(a) && Boolean(b));
+  rl.close();
+});
+
+rl.on("close", () => {
+  process.exit();
+});
