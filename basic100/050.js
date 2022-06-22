a51c95b39d47e1c711b0c19008aaa7782b4a754f
saejinpark951210 <saejinpark951210@gmail.com>
2022-06-23 00:26:41 +0900

    Update 050.js

--- basic100/050.js
+++ basic100/050.js
@@ -1,5 +1,5 @@
 // 두 정수(a, b)를 입력받아
-// a와 b의 값이 같으면 True 를, 같지 않으면 False 를 출력하는 프로그램을 작성해보자.
+// b의 값이 a의 값 보다 크거나 같으면 True 를, 같지 않으면 False 를 출력하는 프로그램을 작성해보자.
 
 const readline = require("readline");
 
@@ -12,7 +12,7 @@ rl.on("line", (line) => {
   let arr = line.split(" ").map((v) => Number(v));
   let a = arr[0];
   let b = arr[1];
-  console.log(a === b);
+  console.log(a <= b);
   rl.close();
 });
 
