f0b592f9ea6c4432f44ff99f86c0a0d09322811f
saejinpark951210 <saejinpark951210@gmail.com>
2022-06-23 00:27:02 +0900

    Create 059.js

---
+++ basic100/059.js
@@ -0,0 +1,37 @@
+// 본 문제는 python 의 빠른 기초 학습을 위해 설계된 문제로서 python 코드 제출을 기준으로 설명되어 있습니다.
+// ------
+
+// 입력 된 정수를 비트단위로 참/거짓을 바꾼 후 정수로 출력해보자.
+// 비트단위(bitwise)연산자 ~ 를 붙이면 된다.(~ : tilde, 틸드라고 읽는다.)
+
+// ** 비트단위(bitwise) 연산자는,
+// ~(bitwise not), &(bitwise and), |(bitwise or), ^(bitwise xor),
+// <<(bitwise left shift), >>(bitwise right shift)
+// 가 있다.
+
+// 예를 들어 1이 입력되었을 때 저장되는 1을 32비트 2진수로 표현하면
+//         00000000 00000000 00000000 00000001 이고,
+// ~1은 11111111 11111111 11111111 11111110 가 되는데 이는 -2를 의미한다.
+
+// 예시
+// a = 1
+// print(~a) #-2가 출력된다.
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (line) => {
+  let a = line;
+  a = ~a;
+
+  console.log(a);
+  rl.close();
+});
+
+rl.on("close", () => {
+  process.exit();
+});
