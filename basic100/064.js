ec45c6e7e5f16c94f8f9c7ea521a9117300d62ea
saejinpark951210 <saejinpark951210@gmail.com>
2022-06-23 15:21:53 +0900

    Create 064.js

---
+++ basic100/064.js
@@ -0,0 +1,20 @@
+// 입력된 세 정수 a, b, c 중 가장 작은 값을 출력하는 프로그램을 작성해보자.
+// 단, 3항 연산을 사용한다.
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (line) => {
+  let arr = line.split(" ").map((v) => Number(v));
+
+  console.log(Math.min(...arr));
+  rl.close();
+});
+
+rl.on("close", () => {
+  process.exit();
+});
