edc1944bd3d1c3cfb3d2753c0260614a8d5e16fc
saejinpark951210 <saejinpark951210@gmail.com>
2022-06-23 15:22:24 +0900

    Create 076.js

---
+++ basic100/076.js
@@ -0,0 +1,25 @@
+// 정수(0 ~ 100) 1개를 입력받아 0부터 그 수까지 순서대로 출력해보자.
+
+// 예시
+// n = int(input())
+// for i in range(n+1) :
+//   print(i)
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (line) => {
+  let a = Number(line);
+  for (let i = 0; i <= a; i++) {
+    console.log(i);
+  }
+  rl.close();
+});
+
+rl.on("close", () => {
+  process.exit();
+});
